75be979b493b5e26b05c8695912461c23d3104e9
Aspectzxcc <josh10nathan@gmail.com>
2025-03-16 10:44:50 +0800

    set create order route to cashier only

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const orderController = require('../controllers/orderController')
+const { authorize } = require('../utils/jwtHandler')
 
 const router = express.Router()
 
-router.post('/', orderController.createOrder)
+router.post('/', authorize('cashier'), orderController.createOrder)
 router.get('/', orderController.getAllOrders)
 router.get('/:id', orderController.getOrderById)
 router.put('/:id', orderController.updateOrder)
